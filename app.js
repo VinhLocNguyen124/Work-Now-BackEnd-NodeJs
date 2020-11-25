57a64dc57e8df1089ffe370aa7bf0b96d1237467
Vinh Loc Nguyen <49594321+VinhLocNguyen124@users.noreply.github.com>
2020-11-26 05:02:53 +0700

    modified app.js

--- app.js
+++ app.js
@@ -6,26 +6,29 @@ const cors = require('cors');
 require('dotenv/config');
 
 //Khi deploy lên heroku thì server sẽ gọi biến PORT ra một cổng ngẫu nhiên, khi dev thì sẽ gọi cổng 3000
+// step 2 -- heroku deployment
 const port = process.env.PORT || 3000;
+
+//Import routes
+const postsRoute = require('./routes/posts');
+const indexRoute = require('./routes'); // --
 
 //Middleware
 app.use(cors());
 app.use(bodyParser.json());
-
-//Import routes
-const postsRoute = require('./routes/posts');
-
 app.use('/posts', postsRoute);
+app.use('/', indexRoute);
 
-//Routes
-app.get('/', (req, res) => {
-    res.send('We are on home');
-});
+// step 3 -- heroku deployment
+if (process.env.NODE_ENV === 'production') {
+
+}
+
 
 
 //TODO: Connect to db here
 mongoose.connect(
-    process.env.DB_CONNECTION,
+    process.env.MONGODB_URI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
