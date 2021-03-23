44dbdc20ac5dc3cbf795586646fb9dec70d09330
Vinh Loc Nguyen <49594321+VinhLocNguyen124@users.noreply.github.com>
2021-03-23 18:42:16 +0700

    Edit some file

--- routes/notifications.js
+++ routes/notifications.js
@@ -64,7 +64,7 @@ router.post('/message', async (req, res) => {
 
         res.json({
             status: "success",
-            time: sendingPeriod
+            time: "hehe"
         });
     } catch (err) {
         res.json({ message: err });
