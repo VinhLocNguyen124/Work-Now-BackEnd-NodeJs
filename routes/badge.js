08d84bb4189d2095dd67861acc5c97351b204437
Vinh Loc Nguyen <49594321+VinhLocNguyen124@users.noreply.github.com>
2021-03-26 02:05:17 +0700

    Edit some file

--- routes/badge.js
+++ routes/badge.js
@@ -26,26 +26,14 @@ router.post('/message/:email', async (req, res) => {
 
             snapshot.forEach(child => {
                 const room = child.val();
-
-                if ((idCurrentUser == room.iduser1 || idCurrentUser == room.iduser2) && room.lastMessage) {
-                    count++;
-                    listId.push({
-                        id1: room.iduser1,
-                        id2: room.iduser2
-                    });
+                if ((idCurrentUser == room.iduser1 || idCurrentUser == room.iduser2) && room.lastMessage && room.unread == true) {
                     badge++;
-                    // if (room.unread === true) {
-                    //     badge = badge + 1;
-                    // }
                 }
             });
         })
 
         res.json({
             badge: badge,
-            count: count,
-            list: listId,
-            id: idCurrentUser
         });
 
     } catch (err) {
